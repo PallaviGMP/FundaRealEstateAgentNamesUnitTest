f9cac2ca506e53d9b2a13a2b9926089dcfa2f868
PallaviGMP <38856906+PallaviGMP@users.noreply.github.com>
2019-12-30 11:04:22 +0100

    unit test

---
+++ HomeControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealEstateAgents.Controllers;
+using RealEstateAgents.Models;
+
+namespace RealEstateAgents.UnitTests
+{
+    [TestClass]
+    public class RealEstateAgentTests
+    {
+        List<RealEstateAgent> realEstateAgent = new List<RealEstateAgent>();
+
+        [TestMethod]
+        public async Task GetRealEstateAgents_SaleInAmsterdam_ReturnsToIndexView()
+        {
+            var homeController = new HomeController();
+
+            var result = await homeController.GetRealEstateAgents("");
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public async Task GetRealEstateAgents_SaleInAmsterdamWithGarden_ReturnsToIndexView()
+        {
+            var homeController = new HomeController();
+
+            var result = await homeController.GetRealEstateAgents("tuin");
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
